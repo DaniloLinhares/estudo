8a4ed37015f96f7932a70829e01c11ee06449103
DaniloLinhares <45154881+DaniloLinhares@users.noreply.github.com>
2019-12-19 22:21:48 -0300

    mundando de puts para expect

--- spec/hello_spec.rb
+++ spec/hello_spec.rb
@@ -5,7 +5,7 @@ describe 'meu primeiro script' do
 
     it 'visitando uma pagina' do
         visit 'https://training-wheels-protocol.herokuapp.com/'
-        puts page.title
+        expect(page.title).to eql 'Training Wheels Protocol'
     end
 
 
